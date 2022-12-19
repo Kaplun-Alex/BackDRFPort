ca7d391fdcb2452d15247dca45724b263a3fe6f7
Alex <kaplunalex1985@gmail.com>
2022-12-19 13:21:57 +0200

    Create app...

--- portcore/portcore/settings.py
+++ portcore/portcore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "portcoreapi.apps.PortcoreapiConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
