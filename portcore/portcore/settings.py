35e17ed815a18c3a00fe9a90f9b1cef1203f5a8f
Alex <kaplunalex1985@gmail.com>
2023-01-31 08:29:58 +0200

    Clear Paths

--- portcore/portcore/settings.py
+++ portcore/portcore/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'portcore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Rfgkey1985+&%$!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
